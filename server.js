9dc9fec8cb03a39845e45caa1b49004687ea2797
mani-tajaddini <mtajaldini@gmail.com>
2018-03-28 23:55:56 +0300

    minor

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ appp.use(bodyParser.urlencoded({extended: false}));
 appp.use(bodyParser.json());
 appp.use(express.static('public'))
 
-appp.listen(process.env.PORT || 8888, function(){
+var server = appp.listen(process.env.PORT || 8888, function(){
   console.log('listening on port 8888')
 })
 
